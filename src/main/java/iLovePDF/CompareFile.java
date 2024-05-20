b23c7a88bd9890f2e5088ab4d068ecd5cd2cf024
Kraut000 <kraut000@gmail.com>
2024-05-20 12:40:43 +0530

    Final compare code

--- src/main/java/iLovePDF/CompareFile.java
+++ src/main/java/iLovePDF/CompareFile.java
@@ -50,27 +50,7 @@ public class CompareFile {
      String sheet15= WorkbookFactory.create(file15).getSheet("Sheet1").getRow(3).getCell(3).getStringCellValue();
      String sheet16= WorkbookFactory.create(file16).getSheet("Sheet1").getRow(4).getCell(3).getStringCellValue();
      
-     System.out.print(sheet1);
-   System.out.print(sheet2);
-   System.out.print(sheet3);
-   System.out.println(sheet4);
 
-   System.out.print(sheet2);
-   System.out.print(sheet6);
-   System.out.print(sheet10);
-   System.out.println(sheet14);
-
-    System.out.print(sheet3);
-    System.out.print(sheet7);
-    System.out.print(sheet11);
-    System.out.println(sheet15);
-
-     System.out.print(sheet4);
-     System.out.print(sheet8);
-     System.out.print(sheet12);
-     System.out.println(sheet16);
-     
-     
      
      FileInputStream file01 = new FileInputStream("C:\\Users\\RK\\Desktop\\New folder\\Excel2.xlsx");
      FileInputStream file02 = new FileInputStream("C:\\Users\\RK\\Desktop\\New folder\\Excel2.xlsx");
@@ -112,293 +92,7 @@ public class CompareFile {
      String sheet015= WorkbookFactory.create(file015).getSheet("Sheet1").getRow(3).getCell(3).getStringCellValue();
      String sheet016= WorkbookFactory.create(file016).getSheet("Sheet1").getRow(4).getCell(3).getStringCellValue();
      
-     System.out.print(sheet01);
-   System.out.print(sheet05);
-   System.out.print(sheet09);
-   System.out.println(sheet013);
 
-   System.out.print(sheet02);
-   System.out.print(sheet06);
-   System.out.print(sheet010);
-   System.out.println(sheet014);
-
-    System.out.print(sheet03);
-    System.out.print(sheet07);
-    System.out.print(sheet011);
-    System.out.println(sheet015);
-
-     System.out.print(sheet04);
-     System.out.print(sheet08);
-     System.out.print(sheet012);
-     System.out.println(sheet016);
-     
-//     String actPN1 = "sheet1";
-//		
-//		String expPN1 = "sheet01";
-//		
-//		if(expPN1!=actPN1)
-//		{
-//		System.out.println("Test Case pass" +" --->  "
-//	
-//		+ sheet1 + sheet01);
-//		}
-//		else
-//		{
-//		System.out.println("fail"  + sheet1 + sheet01);
-//		}
-//
-//
-//
-//	String actPN2 = "sheet2";
-//		
-//		String expPN2 = "sheet02";
-//		
-//		if(expPN2!=actPN2)
-//		{
-//		System.out.println("Test Case pass" +" --->  "
-//	
-//		+ sheet2 + sheet02);
-//		}
-//		else
-//		{
-//		System.out.println("fail"  + sheet2 + sheet02);
-//		}
-//
-//
-//String actPN3 = "sheet3";
-//		
-//		String expPN3 = "sheet03";
-//		
-//		if(expPN3!=actPN3)
-//		{
-//		System.out.println("Test Case pass" +" --->  "
-//	
-//		+ sheet3 + sheet03);
-//		}
-//		else
-//		{
-//		System.out.println("fail"  + sheet3 + sheet03);
-//		}
-//
-//
-//
-//	String actPN4 = "sheet4";
-//		
-//		String expPN4 = "sheet04";
-//		
-//		if(expPN4!=actPN4)
-//		{
-//		System.out.println("Test Case pass" +" --->  "
-//	
-//		+ sheet4 + sheet04);
-//		}
-//		else
-//		{
-//		System.out.println("fail"  + sheet4 + sheet04);
-//		}
-//
-//
-//String actPN5 = "sheet5";
-//		
-//		String expPN5 = "sheet05";
-//		
-//		if(expPN5!=actPN5)
-//		{
-//		System.out.println("Test Case pass" +" --->  "
-//	
-//		+ sheet5 + sheet05);
-//		}
-//		else
-//		{
-//		System.out.println("fail"  + sheet5 + sheet05);
-//		}
-//
-//
-//
-//	String actPN6 = "sheet6";
-//		
-//		String expPN6 = "sheet06";
-//		
-//		if(expPN6!=actPN6)
-//		{
-//		System.out.println("Test Case pass" +" --->  "
-//	
-//		+ sheet6 + sheet06);
-//		}
-//		else
-//		{
-//		System.out.println("fail"  + sheet6 + sheet06);
-//		}
-//
-//
-//String actPN7 = "sheet7";
-//		
-//		String expPN7 = "sheet07";
-//		
-//		if(expPN7!=actPN7)
-//		{
-//		System.out.println("Test Case pass" +" --->  "
-//	
-//		+ sheet7 + sheet07);
-//		}
-//		else
-//		{
-//		System.out.println("fail"  + sheet7 + sheet07);
-//		}
-//
-//
-//
-//	String actPN8 = "sheet8";
-//		
-//		String expPN8 = "sheet08";
-//		
-//		if(expPN8!=actPN8)
-//		{
-//		System.out.println("Test Case pass" +" --->  "
-//	
-//		+ sheet8 + sheet08);
-//		}
-//		else
-//		{
-//		System.out.println("fail"  + sheet8 + sheet08);
-//		}
-//
-//
-//String actPN9 = "sheet9";
-//		
-//		String expPN9 = "sheet09";
-//		
-//		if(expPN9!=actPN9)
-//		{
-//		System.out.println("Test Case pass" +" --->  "
-//	
-//		+ sheet9 + sheet09);
-//		}
-//		else
-//		{
-//		System.out.println("fail"  + sheet9 + sheet09);
-//		}
-//
-//
-//
-//	String actPN10 = "sheet10";
-//		
-//		String expPN10 = "sheet010";
-//		
-//		if(expPN10!=actPN10)
-//		{
-//		System.out.println("Test Case pass" +" --->  "
-//	
-//		+ sheet10 + sheet010);
-//		}
-//		else
-//		{
-//		System.out.println("fail"  + sheet10 + sheet010);
-//		}
-//
-//
-//String actPN11 = "sheet11";
-//		
-//		String expPN11 = "sheet011";
-//		
-//		if(expPN11!=actPN11)
-//		{
-//		System.out.println("Test Case pass" +" --->  "
-//	
-//		+ sheet11 + sheet011);
-//		}
-//		else
-//		{
-//		System.out.println("fail"  + sheet11 + sheet011);
-//		}
-//
-//
-//
-//	String actPN12 = "sheet12";
-//		
-//		String expPN12 = "sheet012";
-//		
-//		if(expPN12!=actPN12)
-//		{
-//		System.out.println("Test Case pass" +" --->  "
-//	
-//		+ sheet12 + sheet012);
-//		}
-//		else
-//		{
-//		System.out.println("fail"  + sheet12 + sheet012);
-//		}
-//
-//
-//String actPN13 = "sheet13";
-//		
-//		String expPN13 = "sheet013";
-//		
-//		if(expPN13!=actPN13)
-//		{
-//		System.out.println("Test Case pass" +" --->  "
-//	
-//		+ sheet13 + sheet013);
-//		}
-//		else
-//		{
-//		System.out.println("fail"  + sheet13 + sheet013);
-//		}
-//
-//
-//
-//	String actPN14 = "sheet14";
-//		
-//		String expPN14 = "sheet014";
-//		
-//		if(expPN14!=actPN14)
-//		{
-//		System.out.println("Test Case pass" +" --->  "
-//	
-//		+ sheet14 + sheet014);
-//		}
-//		else
-//		{
-//		System.out.println("fail"  + sheet14 + sheet014);
-//		}
-//
-//
-//String actPN15 = "sheet15";
-//		
-//		String expPN15 = "sheet015";
-//		
-//		if(expPN15!=actPN15)
-//		{
-//		System.out.println("Test Case pass" +" --->  "
-//	
-//		+ sheet15 + sheet015);
-//		}
-//		else
-//		{
-//		System.out.println("fail"  + sheet15 + sheet015);
-//		}
-//
-//
-//
-//	String actPN16 = "sheet16";
-//		
-//		String expPN16 = "sheet016";
-//		
-//		if(expPN16!=actPN16)
-//		{
-//		System.out.println("Test Case pass" +" --->  "
-//	
-//		+ sheet16 + sheet016);
-//		}
-//		else
-//		{
-//		System.out.println("fail"  + sheet16 + sheet016);
-//		}
-
-   //String actPN1 = "sheet1";
- //	
-// 	String expPN1 = "sheet1";
- 	
  	if(sheet1 != sheet01)
  	{
  	System.out.println("Test Case pass" +" --->  "
@@ -412,9 +106,6 @@ public class CompareFile {
 
 
 
- //String actPN2 = "sheet2";
- //	
-// 	String expPN2 = "sheet02";
  	
  	if(sheet2!=sheet02)
  	{
@@ -428,9 +119,6 @@ public class CompareFile {
  	}
 
 
- //String actPN3 = "sheet3";
- //	
-// 	String expPN3 = "sheet03";
  	
  	if(sheet1!=sheet03)
  	{
@@ -444,10 +132,6 @@ public class CompareFile {
  	}
 
 
-
- //String actPN4 = "sheet4";
- //	
-// 	String expPN4 = "sheet04";
  	
  	if(sheet4!=sheet04)
  	{
@@ -461,9 +145,6 @@ public class CompareFile {
  	}
 
 
- //String actPN5 = "sheet5";
- //	
-// 	String expPN5 = "sheet05";
  	
  	if(sheet5!=sheet05)
  	{
@@ -478,9 +159,6 @@ public class CompareFile {
 
 
 
- //String actPN6 = "sheet6";
- //	
-// 	String expPN6 = "sheet06";
  	
  	if(sheet6!=sheet06)
  	{
@@ -494,9 +172,6 @@ public class CompareFile {
  	}
 
 
- //String actPN7 = "sheet7";
- //	
-// 	String expPN7 = "sheet07";
  	
  	if(sheet7!=sheet07)
  	{
@@ -510,10 +185,6 @@ public class CompareFile {
  	}
 
 
-
- //String actPN8 = "sheet8";
- //	
-// 	String expPN8 = "sheet08";
  	
  	if(sheet8!=sheet08)
  	{
@@ -527,9 +198,6 @@ public class CompareFile {
  	}
 
 
- //String actPN9 = "sheet9";
- //	
-// 	String expPN9 = "sheet09";
  	
  	if(sheet9!=sheet09)
  	{
@@ -542,11 +210,6 @@ public class CompareFile {
  	System.out.println("fail"  + sheet9 + sheet09);
  	}
 
-
-
- //String actPN10 = "sheet10";
- //	
-// 	String expPN10 = "sheet010";
  	
  	if(sheet10!=sheet010)
  	{
@@ -560,11 +223,7 @@ public class CompareFile {
  	}
 
 
- //String actPN11 = "sheet11";
- //	
-// 	String expPN11 = "sheet011";
- 	System.out.println("Start");
- 	if(sheet11==sheet011)
+ 	if(sheet11!=sheet011)
  	{
  	System.out.println("Test Case pass" +" --->  "
 
@@ -576,10 +235,6 @@ public class CompareFile {
  	}
 
 
-
- //String actPN12 = "sheet12";
- //	
-// 	String expPN12 = "sheet012";
  	
  	if(sheet12!=sheet012)
  	{
@@ -593,11 +248,8 @@ public class CompareFile {
  	}
 
 
- //String actPN13 = "sheet13";
- //	
-// 	String expPN13 = "sheet013";
  	
- 	if(sheet13==sheet013)
+ 	if(sheet13!=sheet013)
  	{
  	System.out.println("Test Case pass" +" --->  "
 
@@ -610,11 +262,9 @@ public class CompareFile {
 
 
 
- //String actPN14 = "sheet14";
- //	
-// 	String expPN14 = "sheet014";
+
  	
- 	if(sheet14!=sheet14)
+ 	if(sheet14!=sheet014)
  	{
  	System.out.println("Test Case pass" +" --->  "
 
@@ -626,9 +276,7 @@ public class CompareFile {
  	}
 
 
- //String actPN15 = "sheet15";
- //	
-// 	String expPN15 = "sheet015";
+
  	
  	if(sheet15!=sheet015)
  	{
@@ -642,10 +290,6 @@ public class CompareFile {
  	}
 
 
-
- //String actPN16 = "sheet16";
- //	
-// 	String expPN16 = "sheet016";
  	
  	if(sheet16!=sheet016)
  	{
@@ -661,17 +305,6 @@ public class CompareFile {
 	}
 	
 	
-    public static  void practice(int numer,int number1, String text) throws EncryptedDocumentException, IOException {
-        FileInputStream file1 = new FileInputStream("C:\\Users\\RK\\Desktop\\New folder\\Excel1.xlsx");
-        FileInputStream file2 = new FileInputStream("C:\\Users\\RK\\Desktop\\New folder\\Excel2.xlsx");
-        Sheet sheet= WorkbookFactory.create(file1).getSheet("Sheet1");
-        Row row=sheet.getRow(numer);
-        String text1=row.getCell(number1).getStringCellValue();
-//       String text= cell.getStringCellValue();
-//        double valiue=cell.getNumericCellValue();
-//       
-		
-       
-	}
+
 }
 
